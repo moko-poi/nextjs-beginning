8b0b110373689e1826d74b105648135d5ab5d7d7
b9p31049 <b9p31049@shonan.bunkyo.ac.jp>
2021-10-13 22:22:29 +0900

    blog-page impl

---
+++ components/Post.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+
+type Props = {
+  post: any
+}
+
+const Post: React.FC<Props> = ({ post }) => {
+  return (
+    <div>
+      <span>{post.id}</span>
+      {" : "}
+      <Link href={`/posts/${post.id}`}>
+        <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+          {post.title}
+        </span>
+      </Link>
+    </div>
+  )
+}
+
+export default Post
